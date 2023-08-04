e76a4b343f68d7f6a97996e4b340fa6b25260797
TriggaHappy <19maximilianmerten96@gmail.com>
2023-08-04 11:26:39 +0000

    btn function calling html string

--- calculator.js
+++ calculator.js
@@ -1,1 +1,14 @@
 'use strict';
+
+let operandOne = '';
+let operandTwo = '';
+let sum = '';
+const buttons = document.querySelectorAll('[btn-number]');
+const display = document.getElementById('display');
+
+buttons.forEach(btn => {
+  btn.addEventListener('click', () => {
+    operandOne += btn.textContent;
+    console.log(operandOne);
+  });
+});
