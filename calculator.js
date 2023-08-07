b2404c5bc34ec7c85c364ccb9376bf3582ff23b0
TriggaHappy <19maximilianmerten96@gmail.com>
2023-08-07 15:28:05 +0000

    added firstnumber, operator, part solve, clear fct

--- calculator.js
+++ calculator.js
@@ -1,23 +1,42 @@
 'use strict';
-'strict mode';
 
 //save as strings
 let currentOperand = '';
 let previousOperand = '';
-let opertator = '';
-let sum = '';
+let operator = '';
+let result = '';
 const buttons = document.querySelectorAll('[btn-number]');
+const operators = document.querySelectorAll('[operator]');
 const display = document.getElementById('display');
 
+//first number
 buttons.forEach(btn => {
   btn.addEventListener('click', () => {
     currentOperand += btn.textContent;
-    console.log(currentOperand);
+    operator = '';
   });
 });
 
-//first number
-
-//operation
+//operator
+operators.forEach(btn => {
+  btn.addEventListener('click', () => {
+    operator += btn.textContent;
+    previousOperand += operator;
+    previousOperand += currentOperand;
+    currentOperand = '';
+    //console.log(previousOperand);
+  });
+});
 
 //solve function
+document.querySelector('[solve]').addEventListener('click', function () {
+  previousOperand += currentOperand;
+  console.log(previousOperand);
+});
+
+//clear function
+document.querySelector('[reset]').addEventListener('click', function () {
+  currentOperand = '';
+  previousOperand = '';
+  operator = '';
+});
